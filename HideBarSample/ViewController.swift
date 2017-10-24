4cf5edea735278227edf91b4e1ac4c8b06b53d9c
楊健麟 <derek.y@tc-gaming.co>
2017-10-24 17:44:57 +0800

    [Add] hide bar

--- HideBarSample/ViewController.swift
+++ HideBarSample/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let identifier = "cell"
+    var hidingNavBarManager: HidingNavigationBarManager?
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: identifier)
+        view.addSubview(tableView)
+        
+        self.hidingNavBarManager = HidingNavigationBarManager(viewController: self, scrollView: tableView)
+        if let tabBar = self.tabBarController?.tabBar {
+            self.hidingNavBarManager?.manageBottomBar(tabBar)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +36,36 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate
+{
+    func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+        hidingNavBarManager?.shouldScrollToTop()
+        
+        return true
+    }
+}
+
+extension ViewController: UITableViewDataSource
+{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = "row \((indexPath as NSIndexPath).row)"
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        
+        return cell
+    }
+}
